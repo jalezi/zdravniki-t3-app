1e7a81cac352ca1fde678d6ab276c06eeabbf432
Jaka Daneu <jakad@me.com>
2023-02-03 16:49:30 +0100

    feat: 💄 applies the new css reset
    - change font
    - focus visible style
    - prettier
    - editorconfig file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,27 @@
-import { type AppType } from "next/app";
+import { type AppType } from 'next/app';
 
-import { api } from "../utils/api";
+import { IBM_Plex_Sans } from '@next/font/google';
 
-import "../styles/globals.css";
+import { api } from '../utils/api';
+
+import 'the-new-css-reset';
+import '../styles/globals.css';
+
+const IBMPlexSans = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+  style: ['italic', 'normal'],
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header className={IBMPlexSans.className}>Header</header>
+      <div className={IBMPlexSans.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
