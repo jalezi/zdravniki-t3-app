edf79aa508b75d01608d19023d44c87802734972
Jaka Daneu <jakad@me.com>
2023-02-10 16:36:42 +0100

    feat: ✨ adds Logo component

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import logo from '@/assets/svg/zdravniki-sledilnik-logo.svg?url';
+import type { Locale } from '@/types/i18n';
+
+import styles from './Logo.module.css';
+
+const Logo = () => {
+  const router = useRouter();
+  const locale = router.locale as Locale;
+  return (
+    <span className={styles.container}>
+      <Link href={`/${locale}`}>
+        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+        <Image src={logo as string} fill alt="logo" />
+      </Link>
+    </span>
+  );
+};
+
+export default Logo;
--- src/components/Shared/Icons/index.tsx
+++ src/components/Shared/Icons/index.tsx
@@ -1,1 +1,2 @@
-export { default as IconHamburger } from '@/assets/svg/icon-hamburger.svg';
+export { default as HamburgerSvg } from '@/assets/svg/icon-hamburger.svg';
+export { default as LogoSvg } from '@/assets/svg/zdravniki-sledilnik-logo.svg';
