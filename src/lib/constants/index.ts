620e0632aa512a863b5e5d1df8814ddf757c5ba9
Jaka Daneu <jakad@me.com>
2023-02-18 10:36:51 +0100

    feat: ✨ creates /lib/constants
    + BREAKPOINTS

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,10 @@
+// should match breakpoints in src/lib/styles/variables.css
+export const BREAKPOINTS = {
+  xxs: 0,
+  xs: 375,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+  xxl: 1400,
+} as const;
