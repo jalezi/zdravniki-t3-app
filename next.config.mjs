ff3ef6d3cbfa5e915e464abd62b471e55d7dfbc6
Jaka Daneu <jakad@me.com>
2023-02-20 17:55:37 +0100

    chore: 🐛 poddible wrong svg config
    Haven't noticed any problems, just doesn't look right.

--- next.config.mjs
+++ next.config.mjs
@@ -42,13 +42,13 @@ const config = {
       test: /\.svg$/i,
       type: 'asset',
       resourceQuery: /url/, // *.svg?url
-    }),
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
-        use: ['@svgr/webpack'],
-      });
+    });
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+      use: ['@svgr/webpack'],
+    });
 
     return config;
   },
