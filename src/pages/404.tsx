1446b073eddcfbbb937084e243c1f28eb73429f9
Jaka Daneu <jakad@me.com>
2023-02-20 17:37:37 +0100

    feat: 💄 better custom 404 page
    Translations included.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,45 @@
 import type { GetStaticProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import { SrOnly } from '@/components/Shared/SrOnly';
+import image from '@/../public/assets/images/doctor-404@2x.png';
+import { Button } from '@/components/Shared/Buttons';
 import styles from '@/layouts/LayoutError.module.css';
 
-const NotFoundPage = () => (
-  <>
-    <SrOnly as="h1">404 - Not Found</SrOnly>
+const NotFoundPage = () => {
+  const { t } = useTranslation('common');
+
+  return (
     <div id="error" className={styles.ContainerError}>
-      <h2>404 - Not Found</h2>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className={styles.ContainerError__text_container}>
+        <h1 className={styles.ContainerError__title}>
+          {t('notFound.heading')}
+        </h1>
+        <p className={styles.ContainerError__message}>{t('notFound.text')}</p>
+        <Button
+          as={'a'}
+          href="/"
+          className={styles.ContainerError__button_inside}
+        >
+          {t('navLinks.home')}
+        </Button>
+      </div>
+      <div className={styles.ContainerError__image_container}>
+        <Image alt="not found" src={image} fill />
+      </div>
+      <Button
+        as={Link}
+        passHref
+        href="/"
+        className={styles.ContainerError__button_outside}
+      >
+        {t('navLinks.home')}
+      </Button>
     </div>
-  </>
-);
+  );
+};
 
 export default NotFoundPage;
 
