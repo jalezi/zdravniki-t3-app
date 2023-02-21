c4b32c5a7d2320708a581d806d6e2098496766ee
Jaka Daneu <jakad@me.com>
2023-02-21 13:24:06 +0100

    feat: ✨ adds Map and List icons

--- src/components/Shared/Icons/index.tsx
+++ src/components/Shared/Icons/index.tsx
@@ -4,3 +4,5 @@ export { default as LogoSvg } from '@/assets/svg/zdravniki-sledilnik-logo.svg';
 export { default as CaretDown } from '@/assets/svg/icon-caret-down.svg';
 export { default as CaretUp } from '@/assets/svg/icon-caret-up.svg';
 export { default as LinkSvg } from '@/assets/svg/icon-link.svg';
+export { default as MapSvg } from '@/assets/svg/icon-map-view.svg';
+export { default as ListSvg } from '@/assets/svg/icon-list-view.svg';
