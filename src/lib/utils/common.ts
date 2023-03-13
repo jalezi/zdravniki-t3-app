4dc82c36d990b83e62ffc4c5743622b86d43dd17
Jaka Daneu <jakad@me.com>
2023-03-13 07:09:44 +0100

    feat: ✨ adds formatDate & formatNumber

---
+++ src/lib/utils/common.ts
@@ -0,0 +1,33 @@
+import type { Locale } from '@/lib/types/i18n';
+
+const LocaleLangMap = {
+  sl: 'sl-SL',
+  en: 'en-GB',
+  it: 'it-IT',
+  default: 'sl-SL',
+} satisfies Record<Locale, string>;
+
+export const formatDate = (date: string, locale: Locale = 'sl') => {
+  return new Intl.DateTimeFormat(LocaleLangMap[`${locale}`]).format(
+    new Date(date)
+  );
+};
+
+export const formatNumber = (number: number, locale: Locale = 'sl') => {
+  return new Intl.NumberFormat(LocaleLangMap[`${locale}`]).format(number);
+};
+
+// round on two decimals and locale number
+export const formatNumberRound = (number: number, locale: Locale = 'sl') => {
+  return new Intl.NumberFormat(LocaleLangMap[`${locale}`], {
+    maximumFractionDigits: 2,
+  }).format(number);
+};
+
+// locale percent, max 2 decimals
+export const formatPercent = (number: number, locale: Locale = 'sl') => {
+  return new Intl.NumberFormat(LocaleLangMap[`${locale}`], {
+    style: 'percent',
+    maximumFractionDigits: 2,
+  }).format(number);
+};
