cd80aedcf9be4af5f77354c2ca5ae1bd74987481
Jaka Daneu <jakad@me.com>
2023-03-14 10:07:02 +0100

    feat: ✨ adds Icon size prop

--- src/components/Shared/Icons/Icon.tsx
+++ src/components/Shared/Icons/Icon.tsx
@@ -9,7 +9,21 @@ import { icons } from '.';
 
 export type IconName = keyof typeof icons;
 
-type CustomProps = { name: IconName } | { children: React.ReactNode };
+const IconsStyles = {
+  xxs: styles.Icon__xxs,
+  xs: styles.Icon__xs,
+  sm: styles.Icon__sm,
+  md: styles.Icon__md,
+  lg: styles.Icon__lg,
+  xl: styles.Icon__xl,
+  xxl: styles.Icon__xxl,
+} as const;
+
+export type IconSize = keyof typeof IconsStyles;
+
+type CustomProps =
+  | { name: IconName; size?: IconSize }
+  | { children: React.ReactNode };
 
 export type IconProps = CustomProps &
   Omit<PolymorphicComponentProps<'span'>, 'children'>;
@@ -23,10 +37,11 @@ const Icon = ({ className, ...props }: IconProps) => {
   const iconStyles = clsx(styles.Icon, className);
 
   if ('name' in props) {
-    const { name, ...rest } = props;
+    const { name, size = 'sm', ...rest } = props;
+    const iconStylesWithSize = clsx(iconStyles, IconsStyles[`${size}`]);
     const Svg = icons[`${name}`];
     return (
-      <Polymorphic as="span" className={iconStyles} {...rest}>
+      <Polymorphic as="span" className={iconStylesWithSize} {...rest}>
         <Svg />
       </Polymorphic>
     );
