52965140d558244d809981732ea3695569e184a5
Jaka Daneu <jakad@me.com>
2023-03-14 11:38:05 +0100

    feat: ✨ adda Chip xl and xxl size style

--- src/components/Shared/Chip/Chip.tsx
+++ src/components/Shared/Chip/Chip.tsx
@@ -6,16 +6,24 @@ import type { PolymorphicComponentProps } from '@/components/Shared/Polymorphic'
 import { Polymorphic } from '@/components/Shared/Polymorphic';
 
 import styles from './Chip.module.css';
+import type { IconSize } from '../Icons/Icon';
 
 const SizeStyles = {
   xs: styles.Chip__xs,
   sm: styles.Chip__sm,
   md: styles.Chip__md,
   lg: styles.Chip__lg,
+  xl: styles.Chip__xl,
+  xxl: styles.Chip__xxl,
 } as const;
 
 export type ChipSize = keyof typeof SizeStyles;
-type CustomChipProps = { iconName?: IconName; text: string; size?: ChipSize };
+type CustomChipProps = {
+  iconName?: IconName;
+  text: string;
+  size?: ChipSize;
+  iconSize?: IconSize;
+};
 export type ChipProps = CustomChipProps &
   Omit<PolymorphicComponentProps<'span'>, 'children' | 'as'>;
 
@@ -23,6 +31,7 @@ const Chip = ({
   size = 'sm',
   className,
   iconName,
+  iconSize,
   text,
   ...props
 }: ChipProps) => {
@@ -30,8 +39,8 @@ const Chip = ({
 
   return (
     <Polymorphic as="span" className={chipStyles} {...props}>
-      {iconName ? <Icon name={iconName} /> : null}
-      {text}
+      {iconName ? <Icon name={iconName} size={iconSize ?? size} /> : null}
+      <span>{text}</span>
     </Polymorphic>
   );
 };
