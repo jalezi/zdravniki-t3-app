33f98a8df2457b606c5f8f3b9e8dc209997e69a3
Jaka Daneu <jakad@me.com>
2023-03-15 16:20:17 +0100

    feat: ✨ adds leaflet tooltip
    needs some work. must override a tag leaflet color.

--- src/components/HomeSections/BigMap/BigMap.tsx
+++ src/components/HomeSections/BigMap/BigMap.tsx
@@ -1,5 +1,6 @@
 import { clsx } from 'clsx';
 import { memo } from 'react';
+import { Popup as ReactLeafletPopup } from 'react-leaflet';
 import { useDebounce } from 'usehooks-ts';
 
 import { Map, TotalHits } from '@/components/Shared/Map';
@@ -14,6 +15,9 @@ import { createDoctorFilter } from '@/lib/utils/search';
 
 import styles from './BigMap.module.css';
 import BigMapEvents from './BigMapEvents';
+import { DrActions } from '../DrInfo/DrActions';
+import { DrAvailabilityInfo } from '../DrInfo/DrAvailabilityInfo';
+import { DrBasicInfo } from '../DrInfo/DrBasicInfo';
 
 export type BigMapProps = MapProps;
 
@@ -50,7 +54,50 @@ function withMap(Component: typeof Map) {
             center={center}
             className={markerStyles}
             accepts={doctor.accepts}
-          />
+          >
+            <ReactLeafletPopup>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '1em',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <DrBasicInfo
+                  drId={doctor.fakeId}
+                  name={doctor.name}
+                  href={doctor.href}
+                  isExtra={doctor.isExtra}
+                  address={doctor.location.address.fullAddress}
+                  provider={doctor.provider}
+                  variant="popup"
+                />
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: '1em',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <DrAvailabilityInfo
+                    accepts={doctor.accepts}
+                    availability={doctor.availability}
+                    override={doctor.override}
+                    load={doctor.load}
+                    drId={doctor.fakeId}
+                    variant="popup"
+                  />
+                  <DrActions
+                    drId={doctor.fakeId}
+                    phone={doctor.phone}
+                    variant="popup"
+                  />
+                </div>
+              </div>
+            </ReactLeafletPopup>
+          </DrMarker>
         );
       }) ?? null;
 
