8e7f8c026c115f31822572849c206a1342480f2a
Jaka Daneu <jakad@me.com>
2023-03-18 04:55:47 +0100

    feat: ✨ adds LayoutDoctor

---
+++ src/layouts/LayoutDoctor.tsx
@@ -0,0 +1,28 @@
+import { clsx } from 'clsx';
+
+import { IBMPlexSans } from '@/assets/fonts';
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import { Seo } from '@/components/Seo';
+
+import styles from './LayoutDoctor.module.css';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const LayoutDoctor = (props: Props) => {
+  return (
+    <>
+      <Seo />
+
+      <Header />
+      <main className={clsx(IBMPlexSans.className, styles.LayoutDoctor)}>
+        <div className={clsx(styles.ContainerDoctor)}>{props.children}</div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default LayoutDoctor;
