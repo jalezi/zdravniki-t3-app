077f97c00150b92faff06955224abb8eb5897f79
Jaka Daneu <jakad@me.com>
2023-03-20 16:38:41 +0100

    feat: ✨ makes Seo more generic
    Now Seo component has title, description and keywords props.

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useTranslation } from 'next-i18next';
 
 const defaultSeoData = {
   title: 'Zdravniki - Sledilnik',
@@ -6,19 +7,31 @@ const defaultSeoData = {
     'Zdravniki Sledilnik Slovenija - zbrani, preverjeni, ažurirani, analizirani in pregledni podatki o prostih zdravnikih v Sloveniji',
   keywords: '',
 };
-const socialImgUrl = 'https://zdravniki.sledilnik.org/social.png';
+const SOCIAL_IMG_URL = 'https://zdravniki.sledilnik.org/social.png';
 
-const socialColor = '#09AFDA';
-const canonicalURL = 'https://zdravniki.sledilnik.org/';
+const SOCIAL_COLOR = '#09AFDA';
+const CANONICAL_URL = 'https://zdravniki.sledilnik.org/';
 
-function Seo() {
-  const { title, description, keywords } = defaultSeoData;
+type SeoProps = {
+  title?: string;
+  description?: string;
+  keywords?: string;
+};
+
+function Seo(props: SeoProps) {
+  const { t } = useTranslation('seo');
+
+  const subtitle = t('subtitle') ?? defaultSeoData.title;
+  const title = props.title ? `${props.title} - ${subtitle}` : `${subtitle}`;
+  const description =
+    (props.description || t('description')) ?? defaultSeoData.description;
+  const keywords = (props.keywords || t('keywords')) ?? defaultSeoData.keywords;
 
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
+      {keywords ? <meta name="keywords" content={keywords} /> : null}
       <link rel="icon" href="/favicon.ico" />
       <link
         rel="apple-touch-icon"
@@ -38,19 +51,23 @@ function Seo() {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color={socialColor} />
-      <meta name="msapplication-TileColor" content={socialColor} />
-      <meta name="theme-color" content={socialColor} />
+      <link
+        rel="mask-icon"
+        href="/safari-pinned-tab.svg"
+        color={SOCIAL_COLOR}
+      />
+      <meta name="msapplication-TileColor" content={SOCIAL_COLOR} />
+      <meta name="theme-color" content={SOCIAL_COLOR} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@sledilnik" />
       <meta name="twitter:creator" content="@sledilnik" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={socialImgUrl} />
-      <meta property="og:image" content={socialImgUrl} />
+      <meta name="twitter:image" content={SOCIAL_IMG_URL} />
+      <meta property="og:image" content={SOCIAL_IMG_URL} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <link rel="canonical" href={canonicalURL} />
+      <link rel="canonical" href={CANONICAL_URL} />
     </Head>
   );
 }
