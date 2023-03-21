1b3b3e78d267f0c7a5c73697dbf5bb26b97e36b9
Jaka Daneu <jakad@me.com>
2023-03-21 19:45:07 +0100

    feat: 🥅 wrap main content in default layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,20 +2,32 @@ import { clsx } from 'clsx';
 
 import { IBMPlexSans } from '@/assets/fonts';
 import { Header } from '@/components/Header';
+import { ErrorBoundary, Fallback } from '@/components/Shared/Errors';
 
 import styles from './Layout.module.css';
+import LayoutError from './LayoutError';
 
 type Props = {
   children: React.ReactNode;
+};
+
+const FallbackContainer = () => {
+  return (
+    <LayoutError>
+      <Fallback />
+    </LayoutError>
+  );
 };
 
 const Layout = (props: Props) => {
   return (
     <>
       <Header />
-      <main className={clsx(IBMPlexSans.className, styles.Layout)}>
-        {props.children}
-      </main>
+      <ErrorBoundary fallback={<FallbackContainer />}>
+        <main className={clsx(IBMPlexSans.className, styles.Layout)}>
+          {props.children}
+        </main>
+      </ErrorBoundary>
     </>
   );
 };
