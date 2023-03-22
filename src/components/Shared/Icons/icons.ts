eca6487350a6bf2821ba658f64a96686a7f42daf
Jaka Daneu <jakad@me.com>
2023-03-22 15:43:39 +0100

    fix: 🍱 missing svg and exports

--- src/components/Shared/Icons/icons.ts
+++ src/components/Shared/Icons/icons.ts
@@ -1,12 +1,13 @@
 import AdultsSvg from '@/assets/svg/icon-adults.svg';
+import Alert2Svg from '@/assets/svg/icon-alert-2.svg';
 import AlertSvg from '@/assets/svg/icon-alert.svg';
 import AllSvg from '@/assets/svg/icon-all.svg';
 import BackSvg from '@/assets/svg/icon-back.svg';
 import Ban2Svg from '@/assets/svg/icon-ban-2.svg';
 import BanSvg from '@/assets/svg/icon-ban.svg';
 import BookingSvg from '@/assets/svg/icon-booking.svg';
-import CaretDown from '@/assets/svg/icon-caret-down.svg';
-import CaretUp from '@/assets/svg/icon-caret-up.svg';
+import CaretDownSvg from '@/assets/svg/icon-caret-down.svg';
+import CaretUpSvg from '@/assets/svg/icon-caret-up.svg';
 import CheckCircleSvg from '@/assets/svg/icon-check-circle.svg';
 import CheckSvg from '@/assets/svg/icon-check.svg';
 import ClinicSvg from '@/assets/svg/icon-clinic.svg';
@@ -18,11 +19,14 @@ import EmailSvg from '@/assets/svg/icon-email.svg';
 import FamilyDrSvg from '@/assets/svg/icon-family-dr.svg';
 import FbSvg from '@/assets/svg/icon-fb.svg';
 import FilterSvg from '@/assets/svg/icon-filter.svg';
+import GroupSvg from '@/assets/svg/icon-group.svg';
 import GynSvg from '@/assets/svg/icon-gynecologist.svg';
+import IdCardSvg from '@/assets/svg/icon-id-card.svg';
 import InfoSvg from '@/assets/svg/icon-info.svg';
 import PedSvg from '@/assets/svg/icon-kids.svg';
 import LinkSvg from '@/assets/svg/icon-link.svg';
 import ListSvg from '@/assets/svg/icon-list-view.svg';
+import MapMarkerSvg from '@/assets/svg/icon-map-marker.svg';
 import MapSvg from '@/assets/svg/icon-map-view.svg';
 import PhoneNone from '@/assets/svg/icon-phone-none.svg';
 import Phone from '@/assets/svg/icon-phone.svg';
@@ -34,16 +38,17 @@ import LogoSvg from '@/assets/svg/zdravniki-sledilnik-logo.svg';
 export const icons = {
   AdultsSvg,
   AlertSvg,
+  Alert2Svg,
   AllSvg,
   BackSvg,
   BanSvg,
   Ban2Svg,
   BookingSvg,
-  ClinicSvg,
-  CaretDown,
-  CaretUp,
+  CaretDownSvg,
+  CaretUpSvg,
   CheckSvg,
   CheckCircleSvg,
+  ClinicSvg,
   CloseSvg,
   DentistSvg,
   DotsVert,
@@ -52,12 +57,15 @@ export const icons = {
   FamilyDrSvg,
   FbSvg,
   FilterSvg,
+  GroupSvg,
   GynSvg,
+  IdCardSvg,
   InfoSvg,
   PedSvg,
   LinkSvg,
   ListSvg,
   MapSvg,
+  MapMarkerSvg,
   Phone,
   PhoneNone,
   SearchSvg,
--- src/components/Shared/Icons/index.tsx
+++ src/components/Shared/Icons/index.tsx
@@ -1,4 +1,6 @@
 export { default as AdultsSvg } from '@/assets/svg/icon-adults.svg';
+export { default as AlertSvg } from '@/assets/svg/icon-alert.svg';
+export { default as Alert2Svg } from '@/assets/svg/icon-alert-2.svg';
 export { default as AllSvg } from '@/assets/svg/icon-all.svg';
 export { default as BackSvg } from '@/assets/svg/icon-back.svg';
 export { default as BanSvg } from '@/assets/svg/icon-ban.svg';
@@ -8,6 +10,7 @@ export { default as CaretDownSvg } from '@/assets/svg/icon-caret-down.svg';
 export { default as CaretUpSvg } from '@/assets/svg/icon-caret-up.svg';
 export { default as CheckSvg } from '@/assets/svg/icon-check.svg';
 export { default as CheckCircleSvg } from '@/assets/svg/icon-check-circle.svg';
+export { default as ClinicSvg } from '@/assets/svg/icon-clinic.svg';
 export { default as CloseSvg } from '@/assets/svg/icon-close.svg';
 export { default as DentistSvg } from '@/assets/svg/icon-dentist.svg';
 export { default as DotsVertSvg } from '@/assets/svg/icon-dots-vertical.svg';
@@ -16,8 +19,10 @@ export { default as EmailSvg } from '@/assets/svg/icon-email.svg';
 export { default as FamilyDrSvg } from '@/assets/svg/icon-family-dr.svg';
 export { default as FbSvg } from '@/assets/svg/icon-fb.svg';
 export { default as FilterSvg } from '@/assets/svg/icon-filter.svg';
+export { default as GroupSvg } from '@/assets/svg/icon-group.svg';
 export { default as GynSvg } from '@/assets/svg/icon-gynecologist.svg';
-export { default as ClinicSvg } from '@/assets/svg/icon-clinic.svg';
+export { default as IdCardSvg } from '@/assets/svg/icon-id-card.svg';
+export { default as InfoSvg } from '@/assets/svg/icon-info.svg';
 export { default as PedSvg } from '@/assets/svg/icon-kids.svg';
 export { default as LinkSvg } from '@/assets/svg/icon-link.svg';
 export { default as ListSvg } from '@/assets/svg/icon-list-view.svg';
@@ -28,6 +33,7 @@ export { default as SearchSvg } from '@/assets/svg/icon-search.svg';
 export { default as StudentsSvg } from '@/assets/svg/icon-students.svg';
 export { default as TwSvg } from '@/assets/svg/icon-tw.svg';
 export { default as LogoSvg } from '@/assets/svg/zdravniki-sledilnik-logo.svg';
+export { default as MapMarkerSvg } from '@/assets/svg/icon-map-marker.svg';
 
 export { icons } from './icons';
 
