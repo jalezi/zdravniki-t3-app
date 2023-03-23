7e16be679986da694487cec3bb45d4a637cf0c1d
Jaka Daneu <jakad@me.com>
2023-03-23 16:17:01 +0100

    fix: 🚑️ call multiple nested setState error
    before: click on marker close to edge produce error (not always)
    after: no error

--- src/components/HomeSections/BigMap/BigMapEvents.ts
+++ src/components/HomeSections/BigMap/BigMapEvents.ts
@@ -1,20 +1,48 @@
+import type { LatLngBounds, LatLngTuple } from 'leaflet';
+import { useEffect, useState } from 'react';
 import { useMapEvents } from 'react-leaflet';
+import { useDebounce } from 'usehooks-ts';
 
 import useBoundStore from '@/lib/store/useBoundStore';
 import { getRoundedLatLng } from '@/lib/utils/map';
 
 const BigMapEvents = () => {
-  const setBounds = useBoundStore(state => state.setBounds);
-  const setZoom = useBoundStore(state => state.setZoom);
-  const setCenter = useBoundStore(state => state.setCenter);
+  const [bounds, zoom, center] = useBoundStore(state => [
+    state.bounds,
+    state.zoom,
+    state.center,
+  ]);
+
+  const [state, setState] = useState<{
+    bounds: LatLngBounds | null;
+    zoom: number;
+    center: LatLngTuple;
+  }>({
+    bounds,
+    zoom,
+    center,
+  });
+
+  const debouncedState = useDebounce(state, 500);
+
   const map = useMapEvents({
     moveend: () => {
       const { lat, lng } = getRoundedLatLng(map.getCenter());
-      setBounds(map.getBounds());
-      setZoom(map.getZoom());
-      setCenter([lat, lng]);
+      setState({
+        bounds: map.getBounds(),
+        zoom: map.getZoom(),
+        center: [lat, lng],
+      });
     },
   });
+
+  useEffect(() => {
+    useBoundStore.setState({
+      bounds: debouncedState.bounds,
+      zoom: debouncedState.zoom,
+      center: debouncedState.center,
+    });
+  }, [debouncedState]);
 
   return null;
 };
