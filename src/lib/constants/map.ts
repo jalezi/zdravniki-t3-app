ed2455694106b97561348605d7cde3820e077622
Jaka Daneu <jakad@me.com>
2023-03-25 07:38:32 +0100

    fix: 🩹 random markers bounds too big
    while we need larger bounds to prevent marker's popup not to cause
    error, we want random markers to be in smaller bounds not to fall too
    far from slovenia border.

--- src/lib/constants/map.ts
+++ src/lib/constants/map.ts
@@ -13,21 +13,32 @@ export const LATITUDE = 46.16081 as const;
 export const LONGITUDE = 14.99634 as const;
 export const SL_CENTER: LatLngTuple = [LATITUDE, LONGITUDE];
 
+// slovenia bounds
 export const BOUNDS: {
   southWest: LatLngLiteral;
   northEast: LatLngLiteral;
 } = {
   southWest: {
-    lat: 45.421 - 1,
-    lng: 13.355 - 1,
+    lat: 45.421,
+    lng: 13.355,
   },
   northEast: {
-    lat: 46.894 + 1,
-    lng: 16.637 + 1,
+    lat: 46.894,
+    lng: 16.637,
   },
 };
 
+// correction for map bounds; prevents marker's popup to jump up and down when marker is near the edge of the map(bounds)
+// todo - calculate based on zoom or add minimap so that user can not get lost
+export const BOUNDS_CORRECTION = 1 as const;
+
 export const maxBounds: LatLngBoundsExpression = [
-  [BOUNDS.southWest.lat, BOUNDS.southWest.lng],
-  [BOUNDS.northEast.lat, BOUNDS.northEast.lng],
+  [
+    BOUNDS.southWest.lat - BOUNDS_CORRECTION,
+    BOUNDS.southWest.lng - BOUNDS_CORRECTION,
+  ],
+  [
+    BOUNDS.northEast.lat + BOUNDS_CORRECTION,
+    BOUNDS.northEast.lng + BOUNDS_CORRECTION,
+  ],
 ];
