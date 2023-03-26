7ebf1c04488fffe0d7aac38c611edc2aa9d10348
Jaka Daneu <jakad@me.com>
2023-03-26 18:35:42 +0200

    feat: ✨ adds Portal

---
+++ src/components/Shared/Portal/Portal.tsx
@@ -0,0 +1,43 @@
+import type { ReactNode } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+import styles from './Portal.module.css';
+
+interface PortalProps {
+  children: ReactNode;
+  anchorSelector?: string;
+  noOverlay?: boolean;
+}
+
+const Overlay = ({ children }: { children: ReactNode }) => (
+  <div className={styles.Overlay}>{children}</div>
+);
+
+const Portal = ({
+  children,
+  anchorSelector = '#portal',
+  noOverlay,
+}: PortalProps) => {
+  const ref = useRef<Element | null>(null);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    ref.current = document.querySelector<HTMLElement>(anchorSelector);
+    document.body.style.overflow = 'hidden';
+    setMounted(true);
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [anchorSelector]);
+
+  const overlay = noOverlay ? <> {children} </> : <Overlay>{children}</Overlay>;
+
+  return mounted && ref.current ? createPortal(overlay, ref.current) : null;
+};
+
+Portal.defaultProps = {
+  anchorSelector: '#portal',
+};
+
+export default Portal;
