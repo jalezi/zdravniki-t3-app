74309eb4df658f73fb027e928066911f42dac859
Jaka Daneu <jakad@me.com>
2023-03-27 20:54:39 +0200

    feat: ✨ uses dr report error form

--- src/components/DoctorCard/DoctorCard.tsx
+++ src/components/DoctorCard/DoctorCard.tsx
@@ -1,8 +1,8 @@
 import { clsx } from 'clsx';
 import dynamic from 'next/dynamic';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
+import { useState } from 'react';
 
 import MapSkeleton from '@/components/HomeSections/BigMap/MapSkeleton';
 import { DrAvailabilityInfo } from '@/components/HomeSections/DrInfo/DrAvailabilityInfo';
@@ -19,6 +19,9 @@ import styles from './DoctorCard.module.css';
 import DoctorContacts from './DoctorContacts';
 import DoctorFooter from './DoctorFooter';
 import DoctorOverride from './DoctorOverride';
+import { DoctorReportError } from './DoctorReportError';
+import { Portal } from '../Shared/Portal';
+import { Typography } from '../Shared/Typography';
 
 type DoctorCardProps = {
   doctor: Doctor;
@@ -31,6 +34,8 @@ const DoctorCard = ({ doctor }: DoctorCardProps) => {
   });
 
   const router = useRouter();
+
+  const [edit, setEdit] = useState('edit' in router.query);
   const { t } = useTranslation('common');
   const { t: tDoctor } = useTranslation('doctor');
 
@@ -92,13 +97,7 @@ const DoctorCard = ({ doctor }: DoctorCardProps) => {
           orderformText={tDoctor('orderform.linkReplaceText').toLowerCase()}
         />
 
-        <Button
-          as={Link}
-          href="#"
-          locale={router.locale}
-          passHref
-          container="span"
-        >
+        <Button type="button" onClick={() => setEdit(true)} container="span">
           <Icon name="AlertSvg" size="xxl" />{' '}
           {tDoctor('reportError.linkText').toLowerCase()}
         </Button>
@@ -126,6 +125,29 @@ const DoctorCard = ({ doctor }: DoctorCardProps) => {
       <div id="doctor-map" className={styles.DoctorCard__map}>
         <DrMap position={doctor.location.geoLocation} />
       </div>
+      {edit && (
+        <Portal>
+          <DoctorReportError.Container setEdit={() => setEdit(false)}>
+            <Typography as="h3" element="h2">
+              {tDoctor('reportError.title')}
+            </Typography>
+            <div>
+              <p>{tDoctor('reportError.text')}</p>
+            </div>
+            <DoctorReportError.Form
+              address={doctor.location.address.fullAddress}
+              websites={doctor.websites}
+              phones={doctor.phones}
+              email={doctor.email}
+              orderform={doctor.orderform}
+              accepts={doctor.accepts}
+              availability={doctor.availability}
+              note={doctor.override.note}
+              setEdit={setEdit}
+            />
+          </DoctorReportError.Container>
+        </Portal>
+      )}
     </div>
   );
 };
