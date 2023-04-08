3fda5ee5b223d0ff16cbf32e87710981c02e4eb0
Jaka Daneu <jakad@me.com>
2023-04-08 09:39:05 +0200

    feat: ✨ adds dr report error getMutationInput

---
+++ src/components/DoctorCard/DoctorReportError/utils.ts
@@ -0,0 +1,18 @@
+import type { SendReportInput } from '@/server/api/routers/doctors';
+
+import type { FormData } from './types';
+
+export const getMutationInput = (data: FormData): SendReportInput => {
+  const website = data.websites.map(({ website }) => website).join(',');
+  const phone = data.phones.map(({ phone }) => phone).join(',');
+  return {
+    accepts: data.accepts,
+    address: data.address.trim(),
+    availability: data.availability.trim(),
+    email: data.email.trim(),
+    note: data.note.trim(),
+    phone: phone.trim(),
+    orderform: data.orderform.trim(),
+    website: website.trim(),
+  };
+};
