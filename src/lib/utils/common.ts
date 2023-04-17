b8e55bf941ead3a4bda219df95db1a40d277ffef
Jaka Daneu <jakad@me.com>
2023-04-17 14:50:48 +0200

    feat(utils): ✨ adds getDefaultFontSize()

--- src/lib/utils/common.ts
+++ src/lib/utils/common.ts
@@ -67,3 +67,26 @@ export const fakePromise = async (
     }, timeout);
   });
 };
+
+// https://brokul.dev/detecting-the-default-browser-font-size-in-javascript
+export const getDefaultFontSize = () => {
+  if (typeof window === 'undefined') return null;
+  const element = document.createElement('div');
+  element.style.width = '1rem';
+  element.style.display = 'none';
+  document.body.append(element);
+
+  const widthMatch = window
+    .getComputedStyle(element)
+    .getPropertyValue('width')
+    .match(/\d+/);
+
+  element.remove();
+
+  if (!widthMatch || widthMatch.length < 1) {
+    return null;
+  }
+
+  const result = Number(widthMatch[0]);
+  return !isNaN(result) ? result : null;
+};
