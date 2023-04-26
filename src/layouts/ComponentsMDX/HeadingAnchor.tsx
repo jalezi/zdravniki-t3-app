a172c31586ddf4878b3f9b4a9a6700254997009f
Jaka Daneu <jakad@me.com>
2023-04-26 10:39:48 +0200

    feat(MDX): ✨ adds "a" tag props to HeadingAnchor

--- src/layouts/ComponentsMDX/HeadingAnchor.tsx
+++ src/layouts/ComponentsMDX/HeadingAnchor.tsx
@@ -2,15 +2,16 @@ import { clsx } from 'clsx';
 import { useRef } from 'react';
 
 import { LinkSvg } from '@/components/Shared/Icons';
+import type { PolymorphicComponentProps } from '@/components/Shared/Polymorphic';
 import { Polymorphic } from '@/components/Shared/Polymorphic';
 
 import styles from './ComponentsMDX.module.css';
 
 type HeadingAnchorProps = {
   headingText: string;
-};
+} & Omit<PolymorphicComponentProps<'a'>, 'as'>;
 
-const HeadingAnchor = ({ headingText }: HeadingAnchorProps) => {
+const HeadingAnchor = ({ headingText, ...props }: HeadingAnchorProps) => {
   const ref = useRef<HTMLAnchorElement>(null);
   const href = '#' + headingText.trim().replaceAll(' ', '-').toLowerCase();
   const headingAnchorStyles = clsx(styles.ComponentsMDX, styles.HeadingAnchor);
@@ -23,6 +24,7 @@ const HeadingAnchor = ({ headingText }: HeadingAnchorProps) => {
       className={headingAnchorStyles}
       onClick={() => ref.current?.parentElement?.scrollIntoView()}
       aria-label={`Link to ${headingText}`}
+      {...props}
     >
       <LinkSvg width="1rem" height="1rem" />
     </Polymorphic>
