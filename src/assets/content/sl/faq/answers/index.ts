d190303ce204c50695cf9c4730a7bfbffc63061c
Jaka Daneu <jakad@me.com>
2023-04-30 20:05:58 +0200

    feat(MDX): ✨ adds faq head-count-gyn.mdx

--- src/assets/content/sl/faq/answers/index.ts
+++ src/assets/content/sl/faq/answers/index.ts
@@ -3,5 +3,6 @@ export { default as Availability } from './availability.mdx';
 export { default as DataSource } from './data-source.mdx';
 export { default as DoctorAbsence } from './doctor-absence.mdx';
 export { default as HeadCountGp } from './head-count-gp.mdx';
+export { default as HeadCountGyn } from './head-count-gyn.mdx';
 export { default as FinancingGp } from './financing-gp.mdx';
 export { default as ZZZSData } from './zzzs-data.mdx';
