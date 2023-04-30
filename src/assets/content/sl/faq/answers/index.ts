42b179901bc6f72ecfe82c81d9e79f225c8b7801
Jaka Daneu <jakad@me.com>
2023-04-30 18:51:37 +0200

    feat(MDX): ✨ adds faq financing-gp.mdx

--- src/assets/content/sl/faq/answers/index.ts
+++ src/assets/content/sl/faq/answers/index.ts
@@ -2,4 +2,5 @@ export { default as Accepts } from './accepts.mdx';
 export { default as Availability } from './availability.mdx';
 export { default as DataSource } from './data-source.mdx';
 export { default as DoctorAbsence } from './doctor-absence.mdx';
+export { default as FinancingGp } from './financing-gp.mdx';
 export { default as ZZZSData } from './zzzs-data.mdx';
